f8e174b3b4c22e47d099188c6b9bcc4112e9c936
Daniel-Davies <dd16785@bristol.ac.uk>
2018-03-17 02:24:32 +0000

    with function

--- emotions.py
+++ emotions.py
@@ -1,16 +1,20 @@
 import requests
 
-subscription_key = "c695019b3cca4918b540fc688b7ec516"
-face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
-image_url = 'https://how-old.net/Images/faces2/main007.jpg'
-headers = { 'Ocp-Apim-Subscription-Key': 'c695019b3cca4918b540fc688b7ec516' }
+def make_call(URL):
+    subscription_key = "c695019b3cca4918b540fc688b7ec516"
+    face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
+    image_url = URL
+    headers = {'Ocp-Apim-Subscription-Key': 'c695019b3cca4918b540fc688b7ec516'}
 
-params = {
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 'emotion',
-}
+    params = {
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': 'emotion',
+    }
 
-response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
-faces = response.json()
-print(faces)
+    response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
+    faces = response.json()
+    return faces
+
+
+print(make_call("https://how-old.net/Images/faces2/main007.jpg"))
